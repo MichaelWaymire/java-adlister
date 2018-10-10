8ff88713fa3ab8dc47736cc875cada7844a9b7f5
MichaelWaymire <mike.waymire2@gmail.com>
2018-10-10 13:42:17 -0500

    finsihed quesiton 4, started working on bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,22 +7,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-class siteHitCounter {
-    private int hitCount;
-
-    public void init(){
-        hitCount = 0;
-    }
-}
 
 
 
-@WebServlet (name = "HelloWorldServlet", urlPatterns = "/hello")
+
+@WebServlet (name = "HelloWorldServlet", urlPatterns = "")
 public class HelloWorldServlet extends HttpServlet {
 
+    private int counter = 0;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException, IOException {
 //        Finish up the Hello World Servlet Create a page that displays
 //        "Hello World!" when a user visits /hello
 
@@ -31,22 +26,15 @@ public class HelloWorldServlet extends HttpServlet {
 
         String searchedName = req.getParameter("name");
 
-        if(searchedName == null ) {
+        //if the searchedName is blank it will default to Hell, World!
+        if (searchedName == null) {
             searchedName = "World";
         }
 
         out.println("<h1>Hello, " + searchedName + "</h1>");
 
 
-
-
-
-//        Create a page view counter
-//        Create a page that displays a number that goes up by 1
-//        everytime the /count page is viewed.
-
-
-
-
     }
+
 }
+
---
+++ src/main/java/PageCounterServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "PageCounterServlet", urlPatterns ="/counter")
+public class PageCounterServlet extends HttpServlet {
+
+    private int counter;
+
+    protected void doGet(HttpServletRequest request,HttpServletResponse response)
+        throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        counter++;
+        response.getWriter().println("<h1>This page has a counter</h1>");
+        response.getWriter().println(counter);
+
+    }
+
+
+    //Everything the site refreshes or visited the counter will count up by 1.
+
+
+}
